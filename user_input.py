0a0b28db683e38b4826153848d35e3eb36ea0f9e
timothykungu <161965568+timothykungu@users.noreply.github.com>
2024-03-13 20:00:01 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("Hello " + name + " You're " + age + " years old and live in " + location)
